7548b072ddaad1b83134a2b6515ea03fd3aabfe8
boom1401 <135537981+otsuliini@users.noreply.github.com>
2025-03-14 20:24:11 +0200

    initialize raccoon dating app with main package and user profile structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module raccoon-dating-app
+
+go 1.23.3
+
+require fyne.io/fyne/v2 v2.5.5 // indirect
---
+++ user/profile.go
@@ -0,0 +1,16 @@
+package user
+
+type raccoon struct {
+	// Name of the raccoon
+	Name string
+	// Age of the raccoon
+	Age int
+	// Rizz
+	Rizz string
+	//Aura
+	Aura int
+}
+
+func CreateNewRaccoon(name string, age int, rizz string, aura int) *raccoon {
+	return &raccoon{Name: name, Age: age, Rizz: "rizz", Aura: aura} 
+}	
