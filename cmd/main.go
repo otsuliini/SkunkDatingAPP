2f59e360ccb15e02a76cfa8e962911b98f84f040
boom1401 <135537981+otsuliini@users.noreply.github.com>
2025-03-27 19:20:36 +0200

    feat: implement main function and initialize window for Skunk Dating App

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,9 @@
 package main
 
+import (
+	"SkunkDatingAPP/ui/window" // Correct import path
+)
+
+func main() {
+	window.Window() // Call the function from ui/window
+}
--- ui/window.go
+++ ui/window.go
@@ -1,9 +1,14 @@
-package ui 
+package ui
+
 import (
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
-func window() {
 
+func Window() {
+	skunkDatingApp := app.New()
+	w := skunkDatingApp.NewWindow("Skunk Dating App")
+	w.SetContent(widget.NewLabel("Welcome to the Skunk Dating App!"))
+	w.Show()
+	skunkDatingApp.Run()
 }
